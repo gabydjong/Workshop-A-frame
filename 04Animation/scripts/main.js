405e230ec942976ba20bc3608090c8f1aac987e8
gabydjong <61312829+gabydjong@users.noreply.github.com>
2020-05-14 18:19:14 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -6,7 +6,7 @@
 
  function spin(){
  	myOtherBox.object3D.rotation.x -= rotationSpeed;
- 	myOtherBox.object3D.rotation.y -= rotationSpeed;
+ 	myOtherBox.object3D.rotation.y += rotationSpeed;
  	console.log(myOtherBox.object3D.rotation.x);
  }
 
