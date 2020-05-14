018400873d866399a7799e9c76cb1feac809cf3d
gabydjong <61312829+gabydjong@users.noreply.github.com>
2020-05-14 20:25:47 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -9,6 +9,7 @@
  	myOtherBox.object3D.rotation.y += rotationSpeed;
  	myBox.object3D.scale.set(1, 2, 3);
  	console.log(myOtherBox.object3D.rotation.x);
+ 	//console.log(myBox.object3D.scale.set);
  }
 
  setInterval(spin, 16); //equivalent to 60 fps
